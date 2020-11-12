cf89cd32f4d0af7f7f1e1dd2a601be096f0e68c8
Rafaerl Kinoshita <rafael.kinoshita@gmail.com>
2020-11-12 11:41:03 -0300

    ajuste programas

---
+++ Exemplos/pedidosLanchoneteV1.js
@@ -0,0 +1,96 @@
+var lancheEscolhido = null;
+var querCombo = false;
+var pagarDinheiro = false;
+
+// função para mostrar o menu
+function menu() {
+    console.log("Bem vindo a lanchonete G, nosso menu:");
+    console.log("1 - BiGLanche - R$ 10 (lanche) R$ 14 (combo)");
+    console.log("2 - FranGLanche - R$ 9 (lanche) R$ 13 (combo)");
+    console.log("3 - VeGLanche - R$ 11 (lanche) R$ 16 (combo com suco de beterraba, e chips de aipim)");
+}
+// função para fazer pedido
+function pedidosLanchoneteSimples(lanche, combo, pagamentoDinheiro) {
+    var lancheCarrinho = null;
+    var comboCarrinho = 'lanche';
+    var pagamentoCarrinho = 'cartão';
+    var totalCarrinho = 0;
+    //seletor do lanche
+    switch (lanche) {
+        case 1:
+            lancheCarrinho = "BiGLanche";
+            break;
+        case 2:
+            lancheCarrinho = "FranGLanche";
+            break;
+        case 3:
+            lancheCarrinho = "VeGLanche";
+            break;
+        default:
+            break;
+    }
+    if(combo) {
+        comboCarrinho = 'combo';
+    }
+    if(pagamentoDinheiro) {
+        pagamentoCarrinho = 'dinheiro';
+    }
+
+    if(lancheCarrinho){
+        totalCarrinho = calcularTotalPedido(lanche,combo);
+        console.log('Seu pedido foi um: ' + lancheCarrinho +', Tipo: ' + comboCarrinho + ', total: ' + totalCarrinho + ', pagamento: ' + pagamentoCarrinho);
+    }
+    else {
+        console.log('Algo deu errado com seu pedido verifique o lanche selecionado')
+    }
+}
+// função para mostrar propaganda
+function propagandaTempo(tempo) {
+    for (let index = 0; index < tempo; index++) {
+        if(index === 0){
+            console.log('Seu pedido esta sendo preparado');
+        } else if(index > 0 && index < tempo) {
+            console.log('Obrigado pela preferencia seu pedido ficara pronto logo, aguarde mais um pouco');
+        } else {
+            console.log('Seu pedido esta pronto');
+        }
+    }
+}
+// função para calcular o total do pedido
+function calcularTotalPedido(lanche,combo) {
+    propagandaTempo(3);
+    var valorComboNormal = 4;
+    var valorComboVegano = 5;
+    var valorTotal = 0;
+    switch (lanche) {
+        case 1:
+            valorTotal = 10;
+            if(combo){
+                valorTotal =  valorTotal + valorComboNormal;
+            }
+            break;
+        case 2:
+            valorTotal = 9;
+            if(combo){
+                valorTotal =  valorTotal + valorComboNormal;
+            }
+            break;
+        case 3:
+            valorTotal = 11;
+            if(combo){
+                valorTotal =  valorTotal + valorComboVegano;
+            }
+            break;
+        default:
+            break;
+    }
+    return valorTotal;
+}
+
+// executa programa
+menu();
+lancheEscolhido = 1;
+querCombo = true;
+pagarDinheiro = true
+pedidosLanchoneteSimples(lancheEscolhido,querCombo,pagarDinheiro);
+
